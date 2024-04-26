01011965c9291f50b7b98673e05d5829e8ddf98e
crystaller347 <qwertymum04@gmail.com>
2024-04-26 04:31:39 +0300

    add Profile markup and json data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,17 @@
 import './App.module.css'
+import Profile from "./Profile/Profile.jsx"
+import userData from "../userData.json"
 
 export default function App() {
     return (
-        <></>
-    )
+        <>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
+        </>
+    );
 }
-
-
