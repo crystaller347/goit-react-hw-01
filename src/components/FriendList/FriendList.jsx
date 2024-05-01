e803666e4fdbf9f4b18ef498896d4235645a7524
crystaller347 <qwertymum04@gmail.com>
2024-05-01 16:13:32 +0300

    finish task 2

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,14 @@
+import css from "./FriendList.module.css"
 import FriendListItem from "../FriendListItem/FriendListItem.jsx"
 
 export default function FriendList({ friends }) {
-    <ul>
-        {friends.map((friend) => {
-            return <li key={friend.id}>
-                <FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline} />
-            </li>
-        })}
-    </ul>
+    return (
+        <ul className={css.list}>
+            {friends.map((friend) => {
+                return <li key={friend.id}>
+                    <FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline} />
+                </li>
+            })}
+        </ul>
+    )
 }
