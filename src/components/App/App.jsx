d36dda6953132e2f34b0e77409faf189e223e834
crystaller347 <qwertymum04@gmail.com>
2024-05-01 17:37:24 +0300

    finish task 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import Profile from "../Profile/Profile.jsx"
 import userData from "../../userData.json"
 import FriendList from "../FriendList/FriendList.jsx"
 import friends from "../../friends.json"
+import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx"
+import transactions from "../../transactions.json"
 
 export default function App() {
     return (
@@ -15,6 +17,7 @@ export default function App() {
                 stats={userData.stats}
             />
             <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </>
     );
 }
